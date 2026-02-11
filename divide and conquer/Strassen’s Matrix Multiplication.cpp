64ed23759ac3f041e397032a671b1d5b5d9e69b4
Bhadra <bhadra.korukonda@gmail.com>
2026-02-11 23:48:27 +0530

    1

---
+++ divide and conquer/Strassen’s Matrix Multiplication.cpp
@@ -0,0 +1,48 @@
+Strassen's Matrix Multiplication
+
+import numpy as np
+
+def strassen(A, B):
+    n = len(A)
+    if n == 1:
+        return A * B
+
+    mid = n // 2
+
+    A11 = A[:mid, :mid]
+    A12 = A[:mid, mid:]
+    A21 = A[mid:, :mid]
+    A22 = A[mid:, mid:]
+
+    B11 = B[:mid, :mid]
+    B12 = B[:mid, mid:]
+    B21 = B[mid:, :mid]
+    B22 = B[mid:, mid:]
+
+    M1 = strassen(A11 + A22, B11 + B22)
+    M2 = strassen(A21 + A22, B11)
+    M3 = strassen(A11, B12 - B22)
+    M4 = strassen(A22, B21 - B11)
+    M5 = strassen(A11 + A12, B22)
+    M6 = strassen(A21 - A11, B11 + B12)
+    M7 = strassen(A12 - A22, B21 + B22)
+
+    C11 = M1 + M4 - M5 + M7
+    C12 = M3 + M5
+    C21 = M2 + M4
+    C22 = M1 - M2 + M3 + M6
+
+    C = np.zeros((n, n))
+    C[:mid, :mid] = C11
+    C[:mid, mid:] = C12
+    C[mid:, :mid] = C21
+    C[mid:, mid:] = C22
+
+    return C
+
+
+# Example
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+print(strassen(A, B))
