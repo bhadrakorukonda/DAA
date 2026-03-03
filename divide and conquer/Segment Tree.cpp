b4e0e15dfa63d9745c102de72f72150666328189
Bhadra <bhadra.korukonda@gmail.com>
2026-03-03 22:56:38 +0530

    1

---
+++ divide and conquer/Segment Tree.cpp
@@ -0,0 +1,30 @@
+# Segment Tree for Range Sum
+
+class SegmentTree:
+    def __init__(self, arr):
+        self.n = len(arr)
+        self.tree = [0] * (4 * self.n)
+        self.build(arr, 0, 0, self.n - 1)
+
+    def build(self, arr, node, start, end):
+        if start == end:
+            self.tree[node] = arr[start]
+        else:
+            mid = (start + end) // 2
+            self.build(arr, 2*node+1, start, mid)
+            self.build(arr, 2*node+2, mid+1, end)
+            self.tree[node] = self.tree[2*node+1] + self.tree[2*node+2]
+
+    def query(self, node, start, end, l, r):
+        if r < start or end < l:
+            return 0
+        if l <= start and end <= r:
+            return self.tree[node]
+        mid = (start + end) // 2
+        return (self.query(2*node+1, start, mid, l, r) +
+                self.query(2*node+2, mid+1, end, l, r))
+
+
+arr = [1, 3, 5, 7, 9, 11]
+st = SegmentTree(arr)
+print("Sum in range(1,3):", st.query(0, 0, len(arr)-1, 1, 3))
