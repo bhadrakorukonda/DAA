39b0de70116dad452c372268dff49d6edc006bdd
Bhadra <bhadra.korukonda@gmail.com>
2026-03-03 22:55:54 +0530

    1

---
+++ divide and conquer/Trie (Prefix Tree).cpp
@@ -0,0 +1,40 @@
+# Trie Implementation
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.end = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.end
+
+    def starts_with(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple"))
+print(trie.starts_with("app"))
