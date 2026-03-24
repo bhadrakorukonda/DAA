eab41dd531480e334610e1c960f97a5347b3b8d5
Bhadra <bhadra.korukonda@gmail.com>
2026-03-24 23:57:15 +0530

    1

---
+++ divide and conquer/Disjoint Set Union.cpp
@@ -0,0 +1,30 @@
+# Disjoint Set Union (Union-Find)
+
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0]*n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return
+
+        if self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+        elif self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+        else:
+            self.parent[py] = px
+            self.rank[px] += 1
+
+
+dsu = DSU(5)
+dsu.union(0,1)
+dsu.union(1,2)
+print("Connected(0,2):", dsu.find(0) == dsu.find(2))
