6e5417f1f5a3d1aa936ba4c58284f9681271d79e
Bhadra <bhadra.korukonda@gmail.com>
2026-03-29 23:57:29 +0530

    !

---
+++ divide and conquer/Kruskal’s Algorithm.cpp
@@ -0,0 +1,40 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return False
+        if self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+        else:
+            self.parent[py] = px
+            if self.rank[px] == self.rank[py]:
+                self.rank[px] += 1
+        return True
+
+
+def kruskal(n, edges):
+    edges.sort(key=lambda x: x[2])  # sort by weight
+    dsu = DSU(n)
+    mst_weight = 0
+    mst_edges = []
+
+    for u, v, w in edges:
+        if dsu.union(u, v):
+            mst_weight += w
+            mst_edges.append((u, v, w))
+
+    return mst_weight, mst_edges
+
+
+# Example
+edges = [(0,1,4), (0,2,3), (1,2,1), (1,3,2), (2,3,4)]
+print(kruskal(4, edges))
