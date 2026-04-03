ef15757f5e06fb381941e28c49b9e6f89c34086f
Bhadra <bhadra.korukonda@gmail.com>
2026-04-03 23:41:26 +0530

    !

---
+++ 0/Disjoint Set Union.cpp
@@ -0,0 +1,32 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+
+        if px == py:
+            return False
+
+        if self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+        elif self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+        else:
+            self.parent[py] = px
+            self.rank[px] += 1
+
+        return True
+
+
+# Example
+dsu = DSU(5)
+dsu.union(0, 1)
+dsu.union(1, 2)
+print(dsu.find(2))  # Same set as 0
